f0fd0431285d03f105435d28afe5c626d632dcc6
Jervis Whitley <jervis@whit.com.au>
2016-03-09 21:26:27 +1100

    working task queue

--- kingofthe/main/tasks.py
+++ kingofthe/main/tasks.py
@@ -1,15 +1,17 @@
-from django.utils import timezone as tz
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kingofthe.settings')
+
 from django.conf import settings
+from django.utils import timezone as tz
 
 from celery import Celery
-
-from . import models
 
 app = Celery('tasks')
 app.config_from_object(settings)
 
 @app.task
 def maybe_increment_hit(user_id):
+    from . import models
     now = tz.now()
     delta = tz.timedelta(microseconds=settings.MINIMUM_CHECKIN_MICROSECONDS)
     if models.CheckinHit.objects.filter(user_id=user_id,
